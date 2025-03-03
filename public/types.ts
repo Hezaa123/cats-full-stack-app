e430ed7d8f8420ce71f4a63534f39a29684f930b
Heather Kirwan <heatherkirwan123@hotmail.com>
2025-03-03 15:09:07 +0000

    config and types

---
+++ public/types.ts
@@ -0,0 +1,12 @@
+interface CatProps {
+    id?: number;
+    name: string;
+    image: string;
+    characteristic: string;
+    size: string;
+    furType: string;
+    color: string;
+    description: string;
+   }
+   
+   export type { CatProps }
